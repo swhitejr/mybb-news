98cb25a5534af95fac08744022182750ff811580
Scott White Jr <scwhitejr@gmail.com>
2020-07-05 14:45:49 -0500

    Add WoL info to news

--- Upload/inc/plugins/news.php
+++ Upload/inc/plugins/news.php
@@ -20,3 +20,23 @@ if (defined('IN_ADMINCP')) {
 } else {
     require_once MYBB_ROOT . 'inc/plugins/news/forum.php';
 }
+
+global $plugins;
+
+$plugins->add_hook('fetch_wol_activity_end', 'news_wol');
+function news_wol($user_activity) {
+    if(strpos($user_activity['location'], "news.php"))
+    {
+        $user_activity['activity'] = "news";
+    }
+    return $user_activity;
+}
+
+$plugins->add_hook('build_friendly_wol_location_end', 'news_friendly_loc');
+function news_friendly_loc($array) {
+    if($array['user_activity']['activity'] == "news")
+    {
+        $array['location_name'] = "Viewing <a href='/news.php'>News</a>";
+    }
+    return $array;
+}
